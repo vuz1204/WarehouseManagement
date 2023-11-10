951b18a729c716daf67661e38c31e48b37bfcccd
Nguyễn Văn Vũ <vanvu101204@gmail.com>
2023-11-10 10:19:09 +0700

    Dua du an len remote repository

---
+++ app/src/main/java/fpoly/vunvph33438/warehousemanagement/MainActivity.java
@@ -0,0 +1,14 @@
+package fpoly.vunvph33438.warehousemanagement;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
